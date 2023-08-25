6a32842231fa461c7d55645569d5f6bea7cebf90
kalibetre <kalbetre@icloud.com>
2023-08-26 01:02:18 +0300

    add an abstract BookRepository

---
+++ backend/app/repositories/bookrepository.py
@@ -0,0 +1,28 @@
+import uuid
+from abc import ABC, abstractmethod
+from typing import List
+from app.models.book import Book
+
+class BookRepository(ABC):
+    """
+    A repository for Book instances 
+    """
+    @abstractmethod
+    def save(self, book: Book) -> Book:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def find_one_by_id(self, id: uuid.UUID) -> Book | None:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def find_one_by_title(self, title: str) -> Book | None:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def find_all(self) -> List[Book]:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def delete(self, id: uuid.UUID):
+        raise NotImplementedError()
